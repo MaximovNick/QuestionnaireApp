2649e88b96a8d926ce240344ea0a56692d6908eb
Nikolai Maximov <kolkamaximov@gmail.com>
2022-06-20 10:40:15 +0300

    IntroductionVC

---
+++ Questionnaire/ViewControllers/IntroductionViewController.swift
@@ -0,0 +1,15 @@
+//
+//  IntroductionViewController.swift
+//  Questionnaire
+//
+//  Created by Nikolai Maksimov on 19.06.2022.
+//
+
+import UIKit
+
+class IntroductionViewController: UIViewController {
+
+
+    @IBAction func unwind(for segue: UIStoryboardSegue) {}
+}
+
